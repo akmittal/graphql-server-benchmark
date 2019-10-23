086c5db94c72896cd3b7d24bffc1044bb6dd1366
Amit Mittal <mittalmailbox@gmail.com>
2019-10-23 15:32:56 +0530

    node build

---
+++ node-apollo-server/index.ts
@@ -0,0 +1,59 @@
+
+import knex from 'knex';
+import { createTodoResolver, deleteTodoResolver, getTodosResolver, markCompleteResolver } from './resolvers/todo';
+const { ApolloServer, gql } = require('apollo-server');
+
+
+const connection = knex({
+  client: 'pg',
+  connection:
+  'postgres://postgres:"Oracle@123"@localhost:5432/postgres?sslmode=disable'
+});
+
+const typeDefs = gql`
+  type Todo {
+    id: ID!
+    text: String!
+    done: Boolean!
+  }
+  enum Status {
+    DONE
+    PENDING
+  }
+
+  type Query {
+    todos(type: Status): [Todo!]!
+  }
+
+  input NewTodo {
+    text: String!
+  }
+
+  type Mutation {
+    createTodo(input: NewTodo!): Todo!
+    markComplete(id: ID!): Todo!
+    deleteTodo(id: ID!): Todo!
+  }
+`;
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+  Query: {
+    todos:  getTodosResolver(connection)
+  },
+  Mutation: {
+    createTodo: createTodoResolver(connection),
+    markComplete: markCompleteResolver(connection),
+    deleteTodo: deleteTodoResolver(connection)
+  }
+};
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }:{url:string}) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
---
+++ node-apollo-server/models/Todo.ts
@@ -0,0 +1,47 @@
+import Knex from "knex"
+
+export class Todo {
+    id = ""
+    text = ""
+    done = false
+   
+    constructor(id:string, text:string, done:boolean){
+        this.id = id
+        this.text = text
+        this.done = done
+
+    }
+    async updateTodo(connection:Knex){
+        return connection('todos').where({id:this.id}).update({text:this.text, done:this.done}, ['id', 'text', 'done']);
+
+    }
+    async markComplete(connection:Knex){
+        const todo = await connection('todos').where({id:this.id}).update({done:true},  ['id', 'text', 'done'])
+      
+        return todo[0];
+
+    }
+    async deleteTodo(connection:Knex) {
+        return connection('todos').where({id:this.id}).del();
+    }
+
+
+}
+/**
+ * 
+ * @param {string} id - ID
+ * @param {Knex} connection 
+ */
+export const getByID = async (id:string, connection:Knex): Promise<any> => {
+    const todo = await connection('todos').where({id}).first();
+ 
+    return todo;
+    
+}
+export const allTodos = async (connection:Knex) => {
+    return await connection('todos');
+}
+export const createTodo = async (text:string, connection:Knex) => {
+    const newTodo = await connection('todos').insert({text, done:false}, ['id', 'text','done']);
+    return newTodo[0];
+}
